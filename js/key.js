3a93c8f6f44ba83beb017a0b40fdc9f2f1dee71d
tzuyulien <angie981930@gmail.com>
2015-06-20 22:31:23 +0800

    key change

--- js/key.js
+++ js/key.js
@@ -1,3 +1,2 @@
 //Parse.initialize
-Parse.initialize("8OlfWqZ4fIeGU8sF08dcajpLjnN95qDLA7bBB4NI", "x1b81cXp7lBkQQIciEaCW9Uqvbx3aPWnNl3vsVqV");
-
+Parse.initialize("49I52UfVouMMBtNU15JKjJ0UHn3bpA5bCKrNrBK0", "UmTXciFJLezl4IbKEpVKSa1Ym7HfqSakN8jztTo7");
